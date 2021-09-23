838e502a43ed613c346f692a1df6fcc782f96ed4
Chirag Soni <chiragsoni81245@gmail.com>
2021-09-23 21:58:38 +0530

    controller updated

--- robo_controller.py
+++ robo_controller.py
@@ -11,6 +11,7 @@ try:
             gpio.setup(22, gpio.OUT)
             gpio.setup(23, gpio.OUT)
             gpio.setup(24, gpio.OUT)
+            self.stop()
 
         
         def send_action(self, action):
@@ -36,28 +37,28 @@ try:
             gpio.output(24, False)
 
         def forward(self):
-            gpio.output(17, False)
-            gpio.output(22, True)
+            gpio.output(17, True)
+            gpio.output(22, False)
             gpio.output(23, True)
             gpio.output(24, False)
 
         def reverse(self):
-            gpio.output(17, True)
-            gpio.output(22, False)
+            gpio.output(17, False)
+            gpio.output(22, True)
             gpio.output(23, False)
             gpio.output(24, True)
 
         def left_turn(self):
             gpio.output(17, True)
             gpio.output(22, False)
-            gpio.output(23, True)
+            gpio.output(23, False)
             gpio.output(24, False)
 
         def right_turn(self):
             gpio.output(17, False)
             gpio.output(22, True)
-            gpio.output(23, False)
-            gpio.output(24, True)
+            gpio.output(23, True)
+            gpio.output(24, False)
 
 except ModuleNotFoundError:
     
